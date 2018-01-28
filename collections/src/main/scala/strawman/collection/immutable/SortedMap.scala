4593abbd3417a3594d7e1dc4e684bbbabad87b71
Dhanesh Arole <dhanesh.arole@go-jek.com>
2018-01-28 18:49:53 +0530

    [226] Adds Scaladoc comment on withDefault method of SortedMap

--- collections/src/main/scala/strawman/collection/immutable/SortedMap.scala
+++ collections/src/main/scala/strawman/collection/immutable/SortedMap.scala
@@ -11,6 +11,15 @@ trait SortedMap[K, +V]
     with collection.SortedMap[K, V]
     with SortedMapOps[K, V, SortedMap, SortedMap[K, V]] {
 
+  /** The same sorted map with a given default function.
+    *  Note: The default is only used for `apply`. Other methods like `get`, `contains`, `iterator`, `keys`, etc.
+    *  are not affected by `withDefault`.
+    *
+    *  Invoking transformer methods (e.g. `map`) will not preserve the default value.
+    *
+    *  @param d     the function mapping keys to values, used for non-present keys
+    *  @return      a wrapper of the map with a default value
+    */
   override def withDefault[V1 >: V](d: K => V1): SortedMap[K, V1] = new SortedMap.WithDefault[K, V1](this, d)
 }
 
