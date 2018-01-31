4435bfced384b4538f39ac57c85fd516dfe1bff2
Dhanesh <dhanesh.arole@go-jek.com>
2018-01-31 14:28:18 +0530

    [226] Makes WithDefaultOps private[immutable]

--- collections/src/main/scala/strawman/collection/immutable/WithDefaultOps.scala
+++ collections/src/main/scala/strawman/collection/immutable/WithDefaultOps.scala
@@ -3,7 +3,7 @@ package strawman.collection.immutable
 import strawman.collection.{IterableFactoryLike, MapFactory}
 import scala.{Option}
 
-trait WithDefaultOps[K, +V, +C <: Map[K, V]] { self: C =>
+private[immutable] trait WithDefaultOps[K, +V, +C <: Map[K, V]] { self: C =>
   
   val underlying: C
   
