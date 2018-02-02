2bbc5600426b756473c0485dde46db46c07811ec
Dhanesh <dhanesh.arole@go-jek.com>
2018-02-02 22:38:07 +0530

    [226] Docs on withDefault methods

--- collections/src/main/scala/strawman/collection/immutable/Map.scala
+++ collections/src/main/scala/strawman/collection/immutable/Map.scala
@@ -15,8 +15,26 @@ trait Map[K, +V]
 
   override final def toMap[K2, V2](implicit ev: (K, V) <:< (K2, V2)): Map[K2, V2] = this.asInstanceOf[Map[K2, V2]]
 
+  /** The same map with a given default function.
+    *  Note: The default is only used for `apply`. Other methods like `get`, `contains`, `iterator`, `keys`, etc.
+    *  are not affected by `withDefault`.
+    *
+    *  Invoking transformer methods (e.g. `map`) will not preserve the default value.
+    *
+    *  @param d     the function mapping keys to values, used for non-present keys
+    *  @return      a wrapper of the map with a default value
+    */
   def withDefault[V1 >: V](d: K => V1): MapWithDefault[K, V1] = new MapWithDefaultImpl[K, V1](this, d)
 
+  /** The same map with a given default value.
+    *  Note: The default is only used for `apply`. Other methods like `get`, `contains`, `iterator`, `keys`, etc.
+    *  are not affected by `withDefaultValue`.
+    *
+    *  Invoking transformer methods (e.g. `map`) will not preserve the default value.
+    *
+    *  @param d     default value used for non-present keys
+    *  @return      a wrapper of the map with a default value
+    */
   def withDefaultValue[V1 >: V](d: V1): MapWithDefault[K, V1] = new MapWithDefaultImpl[K, V1](this, _ => d)
 }
 
--- collections/src/main/scala/strawman/collection/immutable/SortedMap.scala
+++ collections/src/main/scala/strawman/collection/immutable/SortedMap.scala
@@ -10,10 +10,26 @@ trait SortedMap[K, +V]
     with collection.SortedMap[K, V]
     with SortedMapOps[K, V, SortedMap, SortedMap[K, V]] {
 
-  /* Override this to provide type signatures of SortedMapWithDefault */
+  /** The same map with a given default function.
+    *  Note: The default is only used for `apply`. Other methods like `get`, `contains`, `iterator`, `keys`, etc.
+    *  are not affected by `withDefault`.
+    *
+    *  Invoking transformer methods (e.g. `map`) will not preserve the default value.
+    *
+    *  @param d     the function mapping keys to values, used for non-present keys
+    *  @return      a wrapper of the map with a default value
+    */
   override def withDefault[V1 >: V](d: K => V1): SortedMapWithDefault[K, V1] = new SortedMapWithDefaultImpl[K, V1](this, d)
 
-  /* Override this to provide type signatures of SortedMapWithDefault */
+  /** The same map with a given default value.
+    *  Note: The default is only used for `apply`. Other methods like `get`, `contains`, `iterator`, `keys`, etc.
+    *  are not affected by `withDefaultValue`.
+    *
+    *  Invoking transformer methods (e.g. `map`) will not preserve the default value.
+    *
+    *  @param d     default value used for non-present keys
+    *  @return      a wrapper of the map with a default value
+    */
   override def withDefaultValue[V1 >: V](d: V1): SortedMapWithDefault[K, V1] = new SortedMapWithDefaultImpl[K, V1](this, _ => d)
 }
 
